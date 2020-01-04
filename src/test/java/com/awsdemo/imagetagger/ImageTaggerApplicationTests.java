0c275b1f04e32675b76b33bfa043c9b58c7cab94
kevinfausett <kevin.fausett@gmail.com>
2020-01-04 12:30:47 -0500

    initial

---
+++ src/test/java/com/awsdemo/imagetagger/ImageTaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsdemo.imagetagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageTaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
