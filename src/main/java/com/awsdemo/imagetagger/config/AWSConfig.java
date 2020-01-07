f36db73c64230d78cd9a382e9ae21a8ce7049989
kevinfausett <kevin.fausett@gmail.com>
2020-01-07 10:54:03 -0500

    DynamoDB config

--- src/main/java/com/awsdemo/imagetagger/config/AWSConfig.java
+++ src/main/java/com/awsdemo/imagetagger/config/AWSConfig.java
@@ -4,20 +4,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 public class AWSConfig {
 
+	@Bean 
+	AmazonRekognition amazonRekognitionClient() {
+		return AmazonRekognitionClientBuilder.standard().build();
+	}
+	
 	@Bean
-	public AmazonS3 amazonS3Client(
-			@Value("${cloud.aws.region.static}") String region,
-			@Value("${aws.access_key_id}") String accessKey, 
-			@Value("${aws.secret_access_key}") String secretKey) {
+	AmazonS3 amazonS3Client(@Value("${cloud.aws.region.static}") String region) {
 		return AmazonS3ClientBuilder.standard().withRegion(region).build();
-		
+	}
+	
+	@Bean
+	AmazonDynamoDB amazonDynamoDBClient(@Value("${cloud.aws.region.static}") String region ){
+		return AmazonDynamoDBClientBuilder.standard().withRegion(region).build();
 	}
 }
