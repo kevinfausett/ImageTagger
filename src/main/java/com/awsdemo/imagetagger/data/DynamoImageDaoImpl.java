40c09c2dd2c18046eb3055adcb63a00b0e6a96c3
kevinfausett <kevin.fausett@gmail.com>
2020-01-09 13:26:40 -0500

    DynamoDBMapper

--- src/main/java/com/awsdemo/imagetagger/data/DynamoImageDaoImpl.java
+++ src/main/java/com/awsdemo/imagetagger/data/DynamoImageDaoImpl.java
@@ -3,15 +3,19 @@ package com.awsdemo.imagetagger.data;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.DeleteItemRequest;
 import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.awsdemo.imagetagger.taggedimage.TaggedImage;
 
 @Service
 public class DynamoImageDaoImpl implements ImageDao {
@@ -27,27 +31,27 @@ public class DynamoImageDaoImpl implements ImageDao {
 
 	@Value("${dynamodb.image_sort_key}")
 	private String sortKey;
-
-	public void putItem(@Value("kevin") String p_key, String s_key, List<String> labels) {
-		PutItemRequest req = new PutItemRequest();
-		req.addItemEntry(primaryKey, new AttributeValue(p_key));
-		req.addItemEntry(sortKey, new AttributeValue(s_key));
-		req.addItemEntry("labels", new AttributeValue(labels));
-		req.setTableName(tableName);
-		amazonDynamoDB.putItem(req);
+	
+	private DynamoDBMapper mapper;
+	
+	@PostConstruct
+	@SuppressWarnings("deprecation")
+	// AWS Docs recommended this class
+	private void mapper() {
+		this.mapper = new DynamoDBMapper(amazonDynamoDB, new DynamoDBMapperConfig(DynamoDBMapperConfig.SaveBehavior.CLOBBER));
 	}
 
-	public String getItem(@Value("kevin") String p_key, String s_key) {
-		GetItemRequest req = new GetItemRequest();
-		HashMap<String, AttributeValue> keys = new HashMap<>();
-		keys.put(primaryKey, new AttributeValue(p_key));
-		keys.put(sortKey, new AttributeValue(s_key));
-		req.setKey(keys);
-		req.setTableName(tableName);
-		return amazonDynamoDB.getItem(req).getItem().get("labels").toString();
+	public void putItem(String p_key, String s_key, List<String> labels) {
+		TaggedImage img = new TaggedImage(s_key, p_key, labels);
+		mapper.save(img);
 	}
 
-	public String deleteItem(@Value("kevin") String p_key, String s_key) {
+	public TaggedImage getItem(String p_key, String s_key) {
+		return mapper.load(TaggedImage.class, p_key, s_key);
+	}
+
+	public String deleteItem(String p_key, String s_key) {
+		// Mapper would require two requests, loading the object and then deleting.
 		DeleteItemRequest req = new DeleteItemRequest();
 		HashMap<String, AttributeValue> keys = new HashMap<>();
 		keys.put(primaryKey, new AttributeValue(p_key));
--- src/main/java/com/awsdemo/imagetagger/taggedimage/TaggedImage.java
+++ src/main/java/com/awsdemo/imagetagger/taggedimage/TaggedImage.java
@@ -2,11 +2,20 @@ package com.awsdemo.imagetagger.taggedimage;
 
 import java.util.List;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName="imagetags")
 public class TaggedImage {
+	@DynamoDBRangeKey(attributeName="s3key")
 	private String key;
 	
+    @DynamoDBHashKey(attributeName="user_id")
 	private String userId;
 	
+    @DynamoDBAttribute(attributeName="labels")
 	private List<String> labels;
 	
 	public TaggedImage(String key, String userId, List<String> labels) {
